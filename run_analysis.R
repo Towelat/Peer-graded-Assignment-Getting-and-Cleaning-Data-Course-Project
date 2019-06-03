a0d3d34e21760d0cc10c4989da07bd74e95ae6bb
Towela <46972618+Towelat@users.noreply.github.com>
2019-06-02 22:58:25 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,10 @@
+
 #files downloaded onto computer
+library(plyr)
+library(dplyr)
 
 #reading in the feature vector
-features<- read.table("features.txt",col.names = c("none","feature_label"))
+features<- read.table("features.txt",col.names = c("value","feature_label"))
 features # to check what data looks like
 
 #reading in activity labels
@@ -13,7 +16,7 @@ activity_labels
 train_x<- read.table("train/X_train.txt", col.names = features$feature_label)
 head(train_x) 
 train_y <- read.table("train/y_train.txt",col.names = "label")
-y_train<-left_join(train_y, activity, by = "label")
+y_train<-join(train_y, activity_labels, by = "label")
 subject_train<- read.table("train/subject_train.txt",col.names = "subject_id")
 head(subject_train)
 
@@ -25,7 +28,7 @@ head(train)
 #the activities in the data set
 test_x <-read.table("test/X_test.txt", col.names = features$feature_label)
 test_y <-read.table("test/y_test.txt", col.names = "label")
-y_test_label <- left_join(test_y, activity, by = "label")
+y_test_label <- join(test_y, activity_labels, by = "label")
 subject_test<- read.table("test/subject_test.txt", col.names = "subject_id")
 
 test<- data.frame(test_x,y_test_label,subject_test)
@@ -36,18 +39,20 @@ combine_data<- rbind(train, test)
 head(combine_data)
 
 #Extracts only the measurements on the mean and standard deviation for each measurement.
-combine_data2 <-combine_data%>% select(contains("mean"), contains("std"))
-
+combine_data2<- combine_data %>% select(matches("mean"), matches("std"))
+head(combine_data2)
 
 #From the data set in step 4, creates a second, independent tidy data set with the 
 #average of each variable for each activity and each subject.
 
-combine_data2$subject_id <- as.factor(combine_data$subject_id)
-combine_data2$activity <- as.factor(combine_data$activity)
+combine_data2$subject_id <- combine_data$subject_id
+combine_data2$activity <- combine_data$activity
 
 
 new_data<- combine_data2 %>% group_by(activity, subject_id)%>% summarise_each(mean)
 new_data
 
 write.table(new_data, file = "tidy_data.txt", row.name= FALSE)
+
+
 
